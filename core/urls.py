4d81e6a940a073765ed8307217356b913a909de3
Przemysław Sosnowski <fogus2000@wp.pl>
2026-01-04 15:44:51 +0100

    Dodanie dzialajacego url /spin/

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+
+
+
+
+
 urlpatterns = [
-path("", views.index, name="index"),
-path("v1/", views.v1, name="view_1")
+path("<int:id>", views.index, name="index"),
+path("spin/", views.spin, name="spin"),
 ]     
--- core/views.py
+++ core/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
-def index(response):
-    return HttpResponse("</h2>Tu narazie jest sciernisko ale bedzie... kasyno</h2>")
-def v1(response):
-    return HttpResponse("<h2>Wersja 1 strony</h2>")
+SYMBOLS = ["🍒", "🍋", "🍏", "🍍", "⭐"]
+
+
+def index(request, id):
+    return HttpResponse("</h1>%s</h1>" % id)
+
+def spin(request):
+    import random
+    result = [random.choice(SYMBOLS) for _ in range(3)]
+    win = len(set(result)) == 1
+
+    return render(request, "core/slot.html", {
+        "result": result,
+        "win": win,
+    })
+
+
+
+
+
+
+
+
+
+
+
+# def spin(request):
+#     import random
+#     result = [random.choice(SYMBOLS) for _ in range(3)]
+#     if len(set(result)) == 1:
+#         message = "Wygrana!"
+#     else:
+#         message = "Spróbuj ponownie!"
+
+#     return HttpResponse(f"{"|".join(result)}<br>{message}")
+
